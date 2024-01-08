1ca505b112d5a66d888f328d14357845b1fb370c
tdindarisky <101049137+tdindarisky@users.noreply.github.com>
2024-01-08 21:56:27 +0700

    Add files via upload

--- deploy_app.py
+++ deploy_app.py
@@ -17,7 +17,7 @@ from wordcloud import WordCloud
 
 
 # Memuat model Naive Bayes
-model = pickle.load(open("model/model_knn.pkl", 'rb'))
+model = pickle.load(open("model/model_naive_bayes.sav", 'rb'))
 
 # Fungsi untuk melakukan prediksi sentimen
 def predict_sentiment(sentiment_text):
